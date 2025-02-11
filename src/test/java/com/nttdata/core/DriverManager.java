3f7395ebaae2db42ac382e0600076459ba254b12
andreaMonica <andreamcondorihuayunga@gmail.com>
2025-02-10 22:20:25 -0500

    j

--- src/test/java/com/nttdata/core/DriverManager.java
+++ src/test/java/com/nttdata/core/DriverManager.java
@@ -3,6 +3,7 @@ package com.nttdata.core;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.time.Duration;
 
@@ -24,17 +26,28 @@ public class DriverManager {
 
     @Before(order = 0)
     public void setUp(){
-        //Se ejecutará Automáticamente
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\Documents\\Drivers\\geckodriver-v0.34.0-win32\\geckodriver.exe");
+        // Usar WebDriverManager para gestionar automáticamente el geckodriver
+        WebDriverManager.firefoxdriver().setup();  // Esto descarga y configura automáticamente el geckodriver
+
+        // Opciones para Firefox
         FirefoxOptions options = new FirefoxOptions();
-        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe"); // Si es necesario especificar la ubicación de Firefox
+
+        // Crear el driver de Firefox con las opciones configuradas
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();  // Maximiza la ventana del navegador
+        wait = new WebDriverWait(driver, Duration.ofSeconds(60));  // Configura la espera explícita
+        //Se ejecutará Automáticamente
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\Documents\\Drivers\\geckodriver-v0.34.0-win32\\geckodriver.exe");
+//        FirefoxOptions options = new FirefoxOptions();
+//        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
         // System.setProperty("webdriver.http.factory", "jdk-http-client");
         //   System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         //  System.setProperty("")
 
-        driver = new FirefoxDriver(options);
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//        driver = new FirefoxDriver(options);
+//        driver.manage().window().maximize();
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
     }
     @Before(order = 1)
     public void setScenario(Scenario scenario){
--- src/test/resources/features/MyStore.feature
+++ src/test/resources/features/MyStore.feature
@@ -1,3 +1,4 @@
+@All
 @MyStore
 
 Feature: Product - Store
