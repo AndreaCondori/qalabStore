c55a552eb6ab606709bc492b992855d79dc517ef
andreaMonica <andreamcondorihuayunga@gmail.com>
2025-02-12 06:49:18 -0500

    drivermanager

--- src/test/java/com/nttdata/core/DriverManager.java
+++ src/test/java/com/nttdata/core/DriverManager.java
@@ -1,4 +1,5 @@
 package com.nttdata.core;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -25,16 +26,18 @@ public class DriverManager {
     @Before(order = 0)
     public void setUp(){
         //Se ejecutará Automáticamente
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\Documents\\Drivers\\geckodriver-v0.34.0-win32\\geckodriver.exe");
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-        // System.setProperty("webdriver.http.factory", "jdk-http-client");
-        //   System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
-        //  System.setProperty("")
-
-        driver = new FirefoxDriver(options);
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\andre\\Documents\\Drivers\\geckodriver-v0.34.0-win32\\geckodriver.exe");
+//        FirefoxOptions options = new FirefoxOptions();
+//        options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+//        // System.setProperty("webdriver.http.factory", "jdk-http-client");
+//        //   System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+//        //  System.setProperty("")
+//
+//        driver = new FirefoxDriver(options);
+//        driver.manage().window().maximize();
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
     @Before(order = 1)
     public void setScenario(Scenario scenario){
